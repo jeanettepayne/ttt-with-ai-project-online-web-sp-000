34f650736d58f955d4033c44c9993636c46bfd25
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-23 16:33:07 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -59,7 +59,7 @@ class Game
   
   def turn
     puts "Please enter a number, 1-9:"
-    input = current_player.move(input)
+    input = current_player.move(board)
   
     if @board.valid_move?(input)
       @board.update(input, current_player)
