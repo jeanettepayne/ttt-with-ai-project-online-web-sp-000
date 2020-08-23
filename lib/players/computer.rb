9f497a11bd674e0ed03b44815afcf1857c5f5b68
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-23 15:32:19 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,23 +3,23 @@ module Players
     attr_reader :cells
     
    def move(board)
-      if board.cells[4] == " "
+      if board.cells[4] == "   "
         "5"
-      elsif board.cells[0] == " "
+      elsif board.cells[0] == "   "
         "1"
-      elsif board.cells[2] == " "
+      elsif board.cells[2] == "   "
         "3"
-      elsif board.cells[6] == " "
+      elsif board.cells[6] == "   "
         "7"
-      elsif board.cells[8] == " "
+      elsif board.cells[8] == "   "
         "9"
-      elsif board.cells[1] == " "
+      elsif board.cells[1] == "   "
         "2"
-      elsif board.cells[3] == " "
+      elsif board.cells[3] == "   "
         "4"
-      elsif board.cells[5] == " "
+      elsif board.cells[5] == "   "
         "6"
-      elsif board.cells[7] == " "
+      elsif board.cells[7] == "   "
         "8"
       end
    end
